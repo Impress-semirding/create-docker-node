5a12c3240778f78e4560bac6a4de7b96f00af4b4
丁薛 <304875853@qq.com>
2021-12-20 11:25:59 +0800

    :art:

--- app/promt.js
+++ app/promt.js
@@ -2,7 +2,7 @@ const promt = [
   {
     type: 'confirm',
     name: 'cool',
-    message: '确认适用自动化部署方案？它将会在项目中生成静态服务器代码及生成dockerfile部署方案'
+    message: '确认使用自动化部署方案？它将会在项目中生成nodejs静态服务器及生成dockerfile部署方案'
   }
 ]
 
